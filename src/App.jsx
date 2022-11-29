e78453bd02c602a75388d9080b27b613c780cce8
Memo Mogollan <gmogollan@gmail.com>
2022-11-29 13:01:32 -0600

    ✨ chore(Hero): first version of hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <div className="bg-primary">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
-  );
-}
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    
+  </div>
+);
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import styles from "../style";
+import { spider } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
+          </h1>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          of superheroes
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Te imaginas, en 20 años, como será la siguiente generación de
+          superhéroes, a nosotros nos tocó vivir un poco de lo pasado, y los
+          nuevo 🕸️
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={spider} alt="marvel" className="w-[100%] h-[100%] relative z-[5]" />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
